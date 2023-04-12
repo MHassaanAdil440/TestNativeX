c27068f148e6c4a1f49b43c1e5ab94306fc8c876
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-04-13 02:01:26 +0500

    Flatlist Practice: Watts App interface using flatlist 70% complete

--- App.js
+++ App.js
@@ -1,71 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   const Data = [
     {
-      key:0,
-      name:"Hassaan",
-      skills:"React native"
+      key: 0,
+      name: "Hassaan",
+      msg: "React native",
+      image: require("./assets/icon.png"),
     },
     {
-      key:1,
-      name:"Najam",
-      skills:"React Js"
+      key: 1,
+      name: "Najam",
+      msg: "React Js",
+      image: require("./assets/icon.png"),
     },
     {
-      key:2,
-      name:"Ali Raza",
-      skills:"MERN"
+      key: 2,
+      name: "Ali Raza",
+      msg: "MERN",
+      image: require("./assets/icon.png"),
     },
     {
-      key:3,
-      name:"Abdan",
-      skills:"Animation & 3D modaling"
+      key: 3,
+      name: "Abdan",
+      msg: "Animation & 3D modaling",
+      image: require("./assets/icon.png"),
     },
     {
-      key:4,
-      name:"Qasim",
-      skills:"Transportataional Engineering"
+      key: 4,
+      name: "Qasim",
+      msg: "Transportataional Engineering",
+      image: require("./assets/icon.png"),
     },
     {
-      key:5,
-      name:"Ammar",
-      skills:"Megatronics and control engineering"
+      key: 5,
+      name: "Ammar",
+      msg: "Megatronics and control engineering",
+      image: require("./assets/icon.png"),
     },
     {
-      key:6,
-      name:"Abdul Hadi",
-      skills:"MERN and React native"
+      key: 6,
+      name: "Abdul Hadi",
+      msg: "MERN and React native",
+      image: require("./assets/icon.png"),
     },
     {
-      key:7,
-      name:"Saqlain",
-      skills:"IT"
+      key: 7,
+      name: "Saqlain",
+      msg: "IT",
+      image: require("./assets/icon.png"),
     },
-  ]
-  const Item = ({title}) => (
+    {
+      key: 8,
+      name: "Arslan",
+      msg: "B.Com",
+      image: require("./assets/icon.png"),
+    },
+    {
+      key: 9,
+      name: "Ammar ch",
+      msg: "CA",
+      image: require("./assets/icon.png"),
+    },
+    {
+      key: 10,
+      name: "Ali",
+      msg: "Dating",
+      image: require("./assets/icon.png"),
+    },
+    {
+      key: 11,
+      name: "Hammad",
+      msg: "CS",
+      image: require("./assets/icon.png"),
+    },
+  ];
+
+  const headings = ["Chat", "Status", "Calls"];
+  const Item = ({ title }) => (
     <View style={styles.item}>
       <Text style={styles.title}>{title}</Text>
     </View>
   );
-
-  // const Item = ({title}) => (
-  //   <View style={styles.item}>
-  //     <Text style={styles.title}>{title}</Text>
-  //   </View>
-  // );
+  const myImage = require('./assets/icon.png');
+  const Img = ({ }) => (
+      <Image source={myImage} style={{height:100, width:100}}/>
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!app</Text>
-      <FlatList 
-        data={Data}
-        renderItem={({item})=><Item title = {item.name}/>}
-      />
-
-{/* <FlatList
-        data={Data}
-        renderItem={({item}) => <Item title={item.name} />} /> */}
+      <View style={styles.topview}>
+        <Text style={{ fontWeight: "bold", fontSize: 25, color: "white" }}>
+          WattsApp
+        </Text>
+        <FlatList
+          data={headings}
+          renderItem={({ item }) => (
+            <View>
+              <Text style={styles.headingstyle}>{item.name}</Text>
+            </View>
+          )}
+          horizontal={true}
+        />
+      </View>
+      <View style={styles.mainview}>
+        <FlatList
+          data={Data}
+          renderItem={({ item }) => (
+            <View>
+              <Img/>
+              <Item title={item.name} />
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -73,15 +121,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  topview: {
+    flex: 0.15,
+    backgroundColor: "green",
+  },
+  mainview: {
+    flex: 0.85,
+    backgroundColor: "white",
   },
   item: {
-    backgroundColor: '#ff6f',
+    backgroundColor: "white",
     padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16,
+    marginHorizontal: 0,
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
   },
   title: {
-    fontSize: 32,
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+  headingstyle: {
+    color: "white",
+    fontWeight: "bold",
+    marginHorizontal: 33,
+    marginTop: 33,
+    fontSize: 20,
   },
 });
