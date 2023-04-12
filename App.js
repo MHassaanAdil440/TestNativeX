3f0e1ce9416cf079e7a1efd6b8ee4eaa6d7da4db
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-04-12 23:19:28 +0500

    Flatlist practice

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+import {CustomFlatlist} from './Components/Flatlist';
 
 export default function App() {
+  // const renderItem = ({ item }) => (
+  //   <Text style={styles.text}>{item.name}</Text>
+  // );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    <View>
+      <CustomFlatlist/>
+      {/* <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      /> */}
     </View>
   );
-}
+{/* }; */}
 
-const styles = StyleSheet.create({
+{/* const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
+    // backgroundColor:'blue'
   },
-});
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+}); */}
+}
