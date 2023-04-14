f71dc9c2bd50a82d704c39b450d4b519692349df
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-04-14 11:13:34 +0500

    Labmid Start

--- App.js
+++ App.js
@@ -1,119 +1,107 @@
-import { StatusBar } from "expo-status-bar";
-import { FlatList, StyleSheet, Text, View, Image } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, FlatList, SectionList, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from "react";
+
+import CustomButton from './CustomButton';
+
+import axios from 'axios'
+
+const DATA = [
+  {
+    title: 'Main dishes',
+    data: ['Pizza', 'Burger', 'Risotto'],
+  },
+  {
+    title: 'Sides',
+    data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+  },
+  {
+    title: 'Drinks',
+    data: ['Water', 'Coke', 'Beer'],
+  },
+  {
+    title: 'Desserts',
+    data: ['Cheese Cake', 'Ice Cream'],
+  },
+];
 
 export default function App() {
-  const Data = [
-    {
-      key: 0,
-      name: "Hassaan",
-      msg: "React native",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 1,
-      name: "Najam",
-      msg: "React Js",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 2,
-      name: "Ali Raza",
-      msg: "MERN",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 3,
-      name: "Abdan",
-      msg: "Animation & 3D modaling",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 4,
-      name: "Qasim",
-      msg: "Transportataional Engineering",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 5,
-      name: "Ammar",
-      msg: "Megatronics and control engineering",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 6,
-      name: "Abdul Hadi",
-      msg: "MERN and React native",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 7,
-      name: "Saqlain",
-      msg: "IT",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 8,
-      name: "Arslan",
-      msg: "B.Com",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 9,
-      name: "Ammar ch",
-      msg: "CA",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 10,
-      name: "Ali",
-      msg: "Dating",
-      image: require("./assets/icon.png"),
-    },
-    {
-      key: 11,
-      name: "Hammad",
-      msg: "CS",
-      image: require("./assets/icon.png"),
-    },
-  ];
 
-  const headings = ["Chat", "Status", "Calls"];
-  const Item = ({ title }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
-  const myImage = require('./assets/icon.png');
-  const Img = ({ }) => (
-      <Image source={myImage} style={{height:100, width:100}}/>
-  );
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+    axios.get('https://fakestoreapi.com/products', {
+      // params: {
+      //     articleID: articleID
+      // }
+  })
+      .then(function (response) {
+          
+          console.log('response',response.data);
+          
+          setData(response.data)
+      
+        })
+      .catch(function (error) {
+          // console.log(error);
+      })
+      .then(function () {
+          // always executed
+          console.log('Always',data[0]);
+      });  
+  },[])
+
+
+  loginBtnPressed=()=>{
+
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.topview}>
-        <Text style={{ fontWeight: "bold", fontSize: 25, color: "white" }}>
-          WattsApp
-        </Text>
-        <FlatList
-          data={headings}
-          renderItem={({ item }) => (
-            <View>
-              <Text style={styles.headingstyle}>{item.name}</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+
+      {/* <CustomButton afzal='top' />
+      <CustomButton afzal='bottom' />
+      <CustomButton afzal='left' />
+      <CustomButton afzal='right' /> */}
+     
+
+      <FlatList
+        data={data}
+        renderItem={({item}) => 
+          (
+            <View style={{flex:1}}>
+               <View style={{flex:0.50, backgroundColor:'lightgrey'}}>
+                  <Text> {item.title} </Text>
+               </View>
+               <View style={{flex:0.50, backgroundColor:'green'}}>
+                  <Text> {item.category} </Text>
+              </View>
+
+              
+              <Text> {item.category} </Text>
             </View>
-          )}
-          horizontal={true}
-        />
-      </View>
-      <View style={styles.mainview}>
-        <FlatList
-          data={Data}
-          renderItem={({ item }) => (
-            <View>
-              <Img/>
-              <Item title={item.name} />
-            </View>
-          )}
-        />
-      </View>
+          )
+        }
+        keyExtractor={item => item.id}
+      />
+
+
+  {/* <SectionList
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({item}) => (
+        <View style={{backgroundColor:'green'}}>
+          <Text style={styles.title}>{item}</Text>
+        </View>
+      )}
+      renderSectionHeader={({section: {title}}) => (
+        <View style={{backgroundColor:'lightgrey'}}>
+        <Text style={styles.header}>{title}</Text>
+        </View>
+      )}
+    /> */}
+
     </View>
   );
 }
@@ -121,33 +109,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  topview: {
-    flex: 0.15,
-    backgroundColor: "green",
-  },
-  mainview: {
-    flex: 0.85,
-    backgroundColor: "white",
-  },
-  item: {
-    backgroundColor: "white",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 0,
-    borderBottomWidth: 1,
-    borderBottomColor: "black",
-  },
-  title: {
-    fontSize: 12,
-    fontWeight: "bold",
-  },
-  headingstyle: {
-    color: "white",
-    fontWeight: "bold",
-    marginHorizontal: 33,
-    marginTop: 33,
-    fontSize: 20,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    marginTop:100
   },
 });
