fd843d9e13a95342aa52d3f8103ad352df8db2da
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-04-19 15:33:36 +0500

    useEffect hook as ComponentDidUpdate life cycle method

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
 import { useEffect, useState } from "react";
-import { View,Text, Button } from "react-native";
+import { View, Text, Button } from "react-native";
 
 export default function App() {
-
   const [int, setInt] = useState(0);
-  useEffect(()=>{
-    console.warn("hello");
-  },[]
-  )
+  const [data, setData] = useState(100);
 
+  // useEffect(() => {
+  //   console.warn("int updated");
+  // }, [int]);
+  useEffect(() => {
+    console.warn("data updated");
+  }, [data,int]);
   return (
     <View>
-      <Text style={{ fontSize: 30, marginTop:30 }}>Life cycle methods with useEffect:{int}</Text>
-      <Button title='update state' onPress={()=>{setInt(int+1)}}/>
-          </View>
+      <Text style={{ fontSize: 30, marginTop: 30 }}>
+        {data} Life cycle methods with useEffect:{int}
+      </Text>
+      <Button
+        title="update int "
+        onPress={() => {
+          setInt(int + 1);
+        }}
+        style={{ margin: 5 }}
+      />
+      <Button
+        title="update data "
+        onPress={() => {
+          setData(data + 1);
+        }}
+        style={{ margin: 5 }}
+      />
+    </View>
   );
 }
