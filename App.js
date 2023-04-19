ecb88b9691fc0c009cdf93cb65a5767de8fe9b7c
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-04-19 15:03:20 +0500

    Life cycle methods with useEffect

--- App.js
+++ App.js
@@ -1,32 +1,18 @@
 import { useEffect, useState } from "react";
-import { FlatList } from "react-native";
-import { View,Text } from "react-native";
+import { View,Text, Button } from "react-native";
 
 export default function App() {
-  const [data, setData] = useState([]);
-  const dataFromApi = async () => {
-    const url = "https://jsonplaceholder.typicode.com/posts";
-    let result = await fetch(url);
-    result =await result.json();
-    setData(result);
-  };
-  useEffect(()=>{dataFromApi()}, []);
 
+  const [int, setInt] = useState(0);
+  useEffect(()=>{
+    console.warn("hello");
+  },[]
+  )
 
   return (
     <View>
-      <Text style={{ fontSize: 30, marginTop:30 }}>Flatlist with API data</Text>
-      {data.length ? (
-        <FlatList
-          data={data}
-          renderItem={({item}) => (
-            <View>
-              <Text>{item.id}</Text>
-              <Text>{item.title}</Text>
-            </View>
-          )}
-        />
-      ) : null}
-    </View>
+      <Text style={{ fontSize: 30, marginTop:30 }}>Life cycle methods with useEffect:{int}</Text>
+      <Button title='update state' onPress={()=>{setInt(int+1)}}/>
+          </View>
   );
 }
